16f132799aed4dcf3d8183bfcb6e3da7cc789e73
Swarnalatha Malkani <155281967+swarnalatha-malkani@users.noreply.github.com>
2025-12-22 12:14:38 +0530

    Add files via upload

---
+++ MAVEN_PROJECTS/hibernate-student/src/main/java/com/skillnext1/App.java
@@ -0,0 +1,29 @@
+package com.skillnext1;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.skillnext1.Student;
+import com.skillnext1.HibernateUtil;
+
+public class App {
+    public static void main(String[] args) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        Student std = new Student(
+                "Mayuri",
+                6,
+                "cse"
+        );
+
+        session.persist(std);
+
+        tx.commit();
+        session.close();
+
+        System.out.println("Student inserted successfully!");
+    }
+}
+
---
+++ MAVEN_PROJECTS/hibernate-student/src/main/java/com/skillnext1/Student.java
@@ -0,0 +1,36 @@
+package com.skillnext1;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private int sem;
+    private String dept;
+
+    public Student() {}
+
+    public Student(String name, int sem, String dept) {
+        this.name = name;
+        this.sem = sem;
+        this.dept = dept;
+    }
+
+    // getters & setters
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getSem() { return sem; }
+    public void setSem(int sem) { this.sem = sem; }
+
+    public String getDept() { return dept; }
+    public void setDept(String dept) { this.dept = dept; }
+}
