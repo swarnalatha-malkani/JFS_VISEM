07024b6380bcb07d23d2adf7e446e804c5472b2a
Swarnalatha Malkani <155281967+swarnalatha-malkani@users.noreply.github.com>
2026-01-06 15:33:14 +0530

    Add files via upload

---
+++ MAVEN_PROJECTS/EmployeeMavenJdbc/src/main/java/com/example/DBUtil.java
@@ -0,0 +1,16 @@
+package com.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil
+{
+
+    public static Connection getConnection() throws Exception 
+  {
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/skillnext_db",
+            "root",
+            "password"
+        );
+    }
+}
